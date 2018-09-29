035db019827ea17ab3fd371e9acfdfd58dd667be
dadsjacsj <43561637+dadsjacsj@users.noreply.github.com>
2018-09-29 09:55:12 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -43,8 +43,8 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           console.log("byCodes:");
-          console.log(byCodes('26478-8'));
-          console.log(byCodes('2345-7'));
+          console.log(byCodes('8462-4'));
+          console.log(byCodes('8480-6'));
 
           var gender = patient.gender;
 
