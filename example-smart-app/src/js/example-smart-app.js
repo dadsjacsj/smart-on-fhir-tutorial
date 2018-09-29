34889742e7b4ba2269d608a1e7d80d04e2120515
dadsjacsj <43561637+dadsjacsj@users.noreply.github.com>
2018-09-29 11:39:12 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,16 +12,16 @@
         var patient = smart.patient;
         var pt = patient.read();
 
-        var obv = smart.patient.api.fetchAll({
-         type: 'Observation',
-          query: {
-           code: {
-             $or: [  "http://loinc.org|8302-2",
-       "http://loinc.org|8462-4",
-     "http://loinc.org|8480-6",
-    "http://loinc.org|2085-9",
-    "http://loinc.org|2089-1",
-     "http://loinc.org|55284-4"]
+//        var obv = smart.patient.api.fetchAll({
+  //       type: 'Observation',
+    //      query: {
+      //     code: {
+        //     $or: [  "http://loinc.org|8302-2",
+   //    "http://loinc.org|8462-4",
+   //  "http://loinc.org|8480-6",
+   // "http://loinc.org|2085-9",
+   // "http://loinc.org|2089-1",
+   //  "http://loinc.org|55284-4"]
            }
          }
         });
@@ -40,15 +40,15 @@
 
         $.when(pt, obv).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
-          console.log("byCodes:");
-          console.log(byCodes('8302-2'));
-          console.log(byCodes('8462-4'));
-          console.log(byCodes('8480-6'));
-          console.log(byCodes('2085-9'));
-          console.log(byCodes('2089-1'));
-          console.log(byCodes('55284-4'));
+     //   $.when(pt, obv).done(function(patient, obv) {
+       //   var byCodes = smart.byCodes(obv, 'code');
+       //   console.log("byCodes:");
+         // console.log(byCodes('8302-2'));
+         // console.log(byCodes('8462-4'));
+         // console.log(byCodes('8480-6'));
+         // console.log(byCodes('2085-9'));
+         // console.log(byCodes('2089-1'));
+         // console.log(byCodes('55284-4'));
 
        $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
@@ -69,11 +69,11 @@
           // Observations
           lymph = byCodes('26478-8');
           // Cerner SoF Tutorial Observations
-          height = byCodes('8302-2');
-          systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          hdl = byCodes('2085-9');
-          ldl = byCodes('2089-1');
+      //    height = byCodes('8302-2');
+       //   systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+        //  diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+        //  hdl = byCodes('2085-9');
+        //  ldl = byCodes('2089-1');
 
 
           var p = defaultPatient();
